a2cc1e02c1423dd1a1ca2a16a9ab61897d5176f0
Charles <CharlesCrowleyDev@gmail.com>
2023-04-25 15:11:24 +0200

    Update 1-Data-types.sol

--- 1-Data-types.sol
+++ 1-Data-types.sol
@@ -14,15 +14,15 @@ contract SimpleStorage {
     We use these to define variables. 
     */
 
-    //bool hasFavouriteNumber = true; // a variable equals true. 
+    bool hasFavouriteNumber = true; // a variable equals true. 
     uint256 favNumber; // we can allocate how much storange to allocate. uint8 (8 bits) default is uint256 (max)
     // can go up by steps of 8. 8, 16, 24, 32... all the way to 256 
     // if you don't give it a value it get set to whatever the null value is, which is 0.
 
-    //string favNumInText = "Three"; // strings are bytes objects but only for text. 
-    //int256 altFavNum = -3;
-    //address myAddress = 0x15719d3bEb0643AE549Fb3cb4bF67A08A198D9f2;
-    //bytes32  favBytes = "cat"; // cat is a string which can automatically get converted into one of these bytes objects. 
+    string favNumInText = "Three"; // strings are bytes objects but only for text. 
+    int256 altFavNum = -3;
+    address myAddress = 0x15719d3bEb0643AE549Fb3cb4bF67A08A198D9f2;
+    bytes32  favBytes = "cat"; // cat is a string which can automatically get converted into one of these bytes objects. 
     // bytes objects typically look like this: 0x2iuh56897ubr95hi4
  
 }
